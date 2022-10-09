4b1315281a866e761e1dc7c13e1e441392e7e17a
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 19:10:30 -0300

    feat(back-end): add get configuration function repository

--- server/src/repositories/configRepository.ts
+++ server/src/repositories/configRepository.ts
@@ -1,2 +1,17 @@
-export async function get(params:any) {
+import { prisma } from "../db/postgresDB";
+
+
+export async function get(userId: number) {
+    const config = await prisma.users.findFirst(
+        {where: {id: userId},
+        select: {
+            firstName: true,
+            lastName: true,
+            description: true,
+            age: true,
+            email: true,
+            password: true
+        }})
+
+        return config;
 }
