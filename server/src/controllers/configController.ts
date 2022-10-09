f7d631fcdf382f3da80ac31acf5357746c93e8a7
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 18:24:51 -0300

    feat(back-end): add get configuration function controller

--- server/src/controllers/configController.ts
+++ server/src/controllers/configController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
-import * as configService from "../services/preferencesServices";
+import * as configService from "../services/configService";
 
 export async function getConfig(req: Request, res: Response){
+    const userId = Number(req.params.id);
 
+    const config = await configService.getConfig(userId);
+    
+    res.status(200).send(config);
 }
---
+++ server/src/services/configService.ts
@@ -0,0 +1,3 @@
+export async function getConfig(params:any) {
+    
+}
