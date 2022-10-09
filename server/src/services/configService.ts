ea8285c454d23878df324fa89e9fb6aae44b7c9c
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 19:05:20 -0300

    feat(back-end): add get configuration function service

--- server/src/services/configService.ts
+++ server/src/services/configService.ts
@@ -1,3 +1,12 @@
-export async function getConfig(params:any) {
-    
+import { prisma } from "../db/postgresDB";
+import * as configRepository from "../repositories/configRepository"
+
+export async function getConfig(userId: number) {
+    const config = await configRepository.get(userId);
+
+    if(!config){
+        throw {type: "Not Found", message: "Configuration data not Found!"};
+    }
+
+    return config;
 }
