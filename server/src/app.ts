0932868b5bf3c3e4fd5d6091dba5febc5c870112
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 04:01:22 -0300

    feat(back-end): add error handler middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandlerMiddleware);
 
 export default app;
