1655236fceef3ea27355e45cc64fc29b79017a97
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 14:40:07 -0300

    feat(back-end): add set preferences repository function

--- server/src/repositories/preferencesRepository.ts
+++ server/src/repositories/preferencesRepository.ts
@@ -2,9 +2,11 @@ import { Preferences } from "@prisma/client";
 import { prisma } from "../db/postgresDB";
 
 export async function findUserPreference(userId: number): Promise<Preferences>{
+    return await prisma.preferences.findFirst({where: {userId}});
 }
 
 export async function createPreference(userId: number, preferences: Partial<Preferences>): Promise<Preferences>{
+    return await prisma.preferences.create({data: {userId, ageRange: preferences.ageRange, focus: preferences.focus}});
 }
 
 export async function setPreferences(id: number, preferences: Partial<Preferences>) {
