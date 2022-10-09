c8450f1a905544436bfa850a0e586e10dd3e20dd
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 18:10:09 -0300

    feat(back-end): add configuration router

---
+++ server/src/routes/configRouter.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const configRouter = Router();
+
+export default configRouter;
--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
+import configRouter from "./configRouter";
 import preferencesRouter from "./preferencesRouter";
 const router = Router();
 
-router.use(authRouter, preferencesRouter);
+router.use(authRouter, preferencesRouter, configRouter);
 
 export default router;
