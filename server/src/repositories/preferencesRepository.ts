899e15fb66d76ccac4f4d3a2c73f9fccbbdfd4e5
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 14:19:30 -0300

    feat(back-end): add set preferences repository function

--- server/src/repositories/preferencesRepository.ts
+++ server/src/repositories/preferencesRepository.ts
@@ -1,3 +1,6 @@
-export async function setPreferences(params:any) {
-    
+import { Preferences } from "@prisma/client";
+import { prisma } from "../db/postgresDB";
+
+export async function setPreferences(id: number, preferences: Partial<Preferences>) {
+    return await prisma.preferences.update({where: {id}, data: preferences});
 }
--- server/src/services/preferencesServices.ts
+++ server/src/services/preferencesServices.ts
@@ -1,12 +1,17 @@
+import { Preferences } from "@prisma/client";
 import * as preferencesRepository from "../repositories/preferencesRepository";
 import * as userRepository from "../repositories/userRepository";
 
-export async function setPreferences(userId: number, preferences: any) {
+export async function setPreferences(userId: number, preferences: Partial<Preferences>) {
     const user = await userRepository.findUserById(userId);
 
     if(!user){
         throw {type: "Not Found", message: "User not Found!"};
     }
 
-    await preferencesRepository.setPreferences(preferences);
+    const update = await preferencesRepository.setPreferences(userId, preferences);
+
+    if(!update){
+        throw {type: "Method not allowed", message: "Update fail!"};
+    }
 }
