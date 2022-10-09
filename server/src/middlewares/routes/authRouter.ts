57f4f661711147be1956c8c758ff7de5d994898e
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 04:26:17 -0300

    feat(back-end): add register users route

--- server/src/middlewares/routes/authRouter.ts
+++ server/src/middlewares/routes/authRouter.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { registerUser } from "../../controllers/authController";
+import { registerSchema } from "../../schemas/authSchemas";
+import schemaValidation from "../schemaValidationMiddleware";
 
 const authRouter = Router();
+
+authRouter.post("/register", schemaValidation(registerSchema), registerUser);
 
 export default authRouter;
