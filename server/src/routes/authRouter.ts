94f4fa6e41be315de3c086947861d977c3ce9058
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 08:35:23 -0300

    feat(back-end): add login users route

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/authController";
-import { registerSchema } from "../schemas/authSchemas";
+import { loginSchema, registerSchema } from "../schemas/authSchemas";
 import schemaValidation from "../middlewares/schemaValidationMiddleware";
 
 const authRouter = Router();
 
 authRouter.post("/register", schemaValidation(registerSchema), registerUser);
+authRouter.post("/login", schemaValidation(loginSchema), );
 
 export default authRouter;
