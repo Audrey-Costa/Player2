7d0744265c87ffe2b53f66e5332eca399cd24136
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 14:39:25 -0300

    feat(back-end): add set preferences service function

--- server/src/repositories/preferencesRepository.ts
+++ server/src/repositories/preferencesRepository.ts
@@ -1,6 +1,12 @@
 import { Preferences } from "@prisma/client";
 import { prisma } from "../db/postgresDB";
 
+export async function findUserPreference(userId: number): Promise<Preferences>{
+}
+
+export async function createPreference(userId: number, preferences: Partial<Preferences>): Promise<Preferences>{
+}
+
 export async function setPreferences(id: number, preferences: Partial<Preferences>) {
     return await prisma.preferences.update({where: {id}, data: preferences});
 }
--- server/src/services/preferencesServices.ts
+++ server/src/services/preferencesServices.ts
@@ -17,5 +17,21 @@ export async function setPreferences(userId: number, preferences: Partial<Prefer
 }
 
 export async function definePreferences(userId: number, preferences: Partial<Preferences>) {
-   
+    const user = await userRepository.findUserById(userId);
+
+    if(!user){
+        throw {type: "Not Found", message: "User not Found!"};
+    }
+
+    const preference = await preferencesRepository.findUserPreference(userId);
+
+    if(preference){
+        throw {type: "Conflict", message: "Preference already saved"};
+    }
+
+    const success = await preferencesRepository.createPreference(userId, preferences);
+
+    if(!success){
+        throw {type: "Method not allowed", message: "Preferences failed to be save!"};
+    }
 }
