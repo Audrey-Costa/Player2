5d7bdf90288b3fb891e0bbc4be84dcea4138d5e1
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 04:16:20 -0300

    feat(back-end): add users types

---
+++ server/src/types/userTypes.ts
@@ -0,0 +1,10 @@
+export type RegisterAuth = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export type LoginAuth = Omit<RegisterAuth, "firstName" | "lastName" | "age" | "confirmPassword">
