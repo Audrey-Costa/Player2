7e356cad060bab838d0aa64202419c07bec4133b
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 08:33:21 -0300

    feat(back-end): add register user repository function

--- server/src/repositories/authRepository.ts
+++ server/src/repositories/authRepository.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../db/postgresDB";
 import { Users } from "@prisma/client";
+import { RegisterAuth } from "../types/userTypes";
 
 export async function findUser(email: string): Promise<Users>{
     const user = await prisma.users.findFirst({where: {email}});
@@ -7,6 +8,6 @@ export async function findUser(email: string): Promise<Users>{
     return user;
 }
 
-export async function registerUser(params:any) {
-    
+export async function registerUser(newUser: RegisterAuth) {
+    await prisma.users.create({data: newUser});
 }
