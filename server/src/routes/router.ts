039f42a6c3a0a7f143148381a78e48ec1264adfd
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 20:31:10 -0300

    feat(back-end): add suggestions router

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 import authRouter from "./authRouter";
 import configRouter from "./configRouter";
 import preferencesRouter from "./preferencesRouter";
+import suggestionsRouter from "./suggestionRouter";
 const router = Router();
 
-router.use(authRouter, preferencesRouter, configRouter);
+router.use(authRouter, preferencesRouter, configRouter, suggestionsRouter);
 
 export default router;
---
+++ server/src/routes/suggestionRouter.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const suggestionsRouter = Router();
+
+export default suggestionsRouter;
