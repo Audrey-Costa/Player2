2f51cefe2ccf5e14c07e07906fe67786a86ec151
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 13:34:17 -0300

    feat(back-end): add preferences router

---
+++ server/src/controllers/preferencesController.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export async function setPreferences(req: Request, res: Response){
+    
+}
--- server/src/routes/preferencesRouter.ts
+++ server/src/routes/preferencesRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { setPreferences } from "../controllers/preferencesController";
+import { validateToken } from "../middlewares/validateToken";
 
 const preferencesRouter = Router();
+
+preferencesRouter.put("/preferences", validateToken, setPreferences)
 
 export default preferencesRouter;
