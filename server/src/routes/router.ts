d8df872d449408f72f5d58daad477cd6eef8d3f7
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-10 00:29:19 -0300

    feat(back-end): add userRouter

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -3,8 +3,9 @@ import authRouter from "./authRouter";
 import configRouter from "./configRouter";
 import preferencesRouter from "./preferencesRouter";
 import suggestionsRouter from "./suggestionRouter";
+import userRouter from "./userRouter";
 const router = Router();
 
-router.use(authRouter, preferencesRouter, configRouter, suggestionsRouter);
+router.use(authRouter, preferencesRouter, configRouter, suggestionsRouter, userRouter);
 
 export default router;
