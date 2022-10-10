24f5ed65f26250d0ddf51af250df0a83e2d957e7
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 23:12:09 -0300

    feat(back-end): add suggestions get repository

--- server/src/repositories/suggestionsRepository.ts
+++ server/src/repositories/suggestionsRepository.ts
@@ -1,7 +1,57 @@
-export async function getRandomSuggestions(params:any): Promise<[]> {
-    return []
+import { Preferences } from "@prisma/client";
+import { prisma } from "../db/postgresDB"
+
+export async function getRandomSuggestions(userId: number){
+    const suggestions = await prisma.users.findMany(
+        {
+            where: {
+                id: {
+                    not: userId
+                }
+            },
+            select:{
+                firstName: true,
+                lastName: true,
+                age: true,
+                description: true,
+                preferences: {select: {focus: true}}
+            }
+        }
+    );
+        
+    return suggestions;
 }
 
-export async function getSuggestions(params:any): Promise<[]> {
-    return []
+export async function getSuggestions(userId: number, preferences: Preferences, ageRange: [string, string]){
+    const suggestions = await prisma.users.findMany(
+        {
+            where: {
+                id: {
+                    not: userId
+                },
+                age: {
+                    gte: Number(ageRange[0]), lte: Number(ageRange[1])
+                },
+                preferences: {
+                    some: {
+                        focus: {
+                            equals: preferences.focus
+                        }
+                    }
+                }
+            },
+            include: {
+                preferences: {
+                    select:{
+                        focus: true
+                    }, 
+                    where:{
+                        focus: {equals: preferences.focus}
+                    }
+                }
+            }
+        }
+    );
+    
+    return suggestions;
 }
--- server/src/services/suggestionsService.ts
+++ server/src/services/suggestionsService.ts
@@ -1,19 +1,22 @@
-import * as suggestionsRepository from "../repositories/suggestionsRepository"
+import * as suggestionsRepository from "../repositories/suggestionsRepository";
+import * as preferencesRepository from "../repositories/preferencesRepository";
 
 export async function getSuggestions(userId: number, random: any) {
     if(random){
         const suggestions = await suggestionsRepository.getRandomSuggestions(userId);
 
-        if(suggestions.length){
+        if(suggestions.length === 0){
             throw {type: "Not Found", message: "There are no users to suggest..."};
         }
 
         return suggestions;
-        
+
     }else{
-        const suggestions = await suggestionsRepository.getSuggestions(userId);
+        const preferences = await preferencesRepository.findUserPreference(userId);
+        const ageRange: [string, string] = <[string, string]>preferences.ageRange.split("-");
+        const suggestions = await suggestionsRepository.getSuggestions(userId, preferences, ageRange);
         
-        if(suggestions.length){
+        if(suggestions.length === 0){
             throw {type: "Not Found", message: "There are no users to suggest..."};
         }
         
