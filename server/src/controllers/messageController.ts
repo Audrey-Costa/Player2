965e4c17ea8ea890491336df49c9739384ca63f0
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-10 01:55:26 -0300

    feat(back-end): add send message route

---
+++ server/src/controllers/messageController.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from "express";
+
+export async function sendMessage(req: Request, res: Response) {
+}
--- server/src/routes/messageRouter.ts
+++ server/src/routes/messageRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { validateToken } from "../middlewares/validateToken";
+import { sendMessage } from "../controllers/messageController";
 
 const messageRouter = Router();
+
+messageRouter.post("/:id/message/send", validateToken, sendMessage);
 
 export default messageRouter;
