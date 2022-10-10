626cfdfd1432b80b1386c5dd30ce8c09f91cd214
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 23:22:17 -0300

    fix(back-end): reduce the user information get

--- server/src/repositories/userRepository.ts
+++ server/src/repositories/userRepository.ts
@@ -1,8 +1,20 @@
 import { prisma } from "../db/postgresDB";
 import { Users } from "@prisma/client";
 
-export async function findUserById(id: number): Promise<Users> {
-    const user = await prisma.users.findFirst({where: {id}});
+export async function findUserById(id: number): Promise<Partial<Users>> {
+    const user = await prisma.users.findFirst(
+        {
+            where: {
+                id
+            },
+            select: {
+                firstName: true,
+                lastName: true,
+                age: true,
+                description: true
+            }
+        }
+    );
 
     return user;
 }
