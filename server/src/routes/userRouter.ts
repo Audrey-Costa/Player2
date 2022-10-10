959c23559b6749d5784095744e341ce16b8dd27e
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-10 00:32:50 -0300

    feat(back-end): add get profile route

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { getProfile } from "../controllers/userController";
+import { validateToken } from "../middlewares/validateToken";
 
 const userRouter = Router();
+
+userRouter.get("/profile", validateToken, getProfile);
 
 export default userRouter;
