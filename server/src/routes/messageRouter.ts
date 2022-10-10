b65c4dbe946e5d09cd019bbe38b4fe38f93c1edf
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-10 08:01:46 -0300

    fix(back-end): fix the message verification

--- server/src/routes/messageRouter.ts
+++ server/src/routes/messageRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/validateToken";
 import { sendMessage, getMessages } from "../controllers/messageController";
+import schemaValidation from "../middlewares/schemaValidationMiddleware";
+import { messageSchema } from "../schemas/messageSchema";
 
 const messageRouter = Router();
 
-messageRouter.post("/:id/message/send", validateToken, sendMessage);
+messageRouter.post("/:id/message/send", validateToken, schemaValidation(messageSchema),sendMessage);
 messageRouter.get("/:id/message/:friendId", validateToken, getMessages);
 
 export default messageRouter;
