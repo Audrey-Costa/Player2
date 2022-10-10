feefb1e370217a208003d40e32a3af220f9f4bcd
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-10 01:09:50 -0300

    fix(back-end): change get profile route

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import * as usersService from "../services/usersService";
 
 export async function getProfile(req: Request, res: Response) {
-    const userId = Number(req.params.id);
+    const userId = Number(req.params.userId);
 
     const profile = await usersService.getProfile(userId);
 
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -4,6 +4,8 @@ import { validateToken } from "../middlewares/validateToken";
 
 const userRouter = Router();
 
-userRouter.get("/profile/:id", validateToken, getProfile);
+userRouter.get("/profile/:id/:userId", validateToken, getProfile);
+
+
 
 export default userRouter;
