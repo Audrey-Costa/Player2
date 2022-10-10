0a6e1a7839ac948c87363988c5125e42e256dc55
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-10 04:02:45 -0300

    feat(back-end): add get messages controller

--- server/src/controllers/messageController.ts
+++ server/src/controllers/messageController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { TypeMessage } from "../types/messageType";
 import * as messageServices from "../services/messageServices";
 
-export async function sendMessage(req: Request, res: Response) {
+export async function sendMessage(req: Request, res: Response){
     const message: TypeMessage = req.body;
 
     await messageServices.sendMessage(message);
@@ -10,5 +10,11 @@ export async function sendMessage(req: Request, res: Response) {
     res.status(201);
 }
 
-export async function getMessages(req: Request, res: Response) {
+export async function getMessages(req: Request, res: Response){
+    const userId = Number(req.params.id);
+    const friendId = Number(req.params.friendId);
+
+    const messages = await messageServices.getMessages(userId, friendId);
+
+    res.status(200).send(messages);
 }
--- server/src/services/messageServices.ts
+++ server/src/services/messageServices.ts
@@ -1,10 +1,14 @@
 import { TypeMessage } from "../types/messageType";
 import * as messageRepository from "../repositories/messageRepository";
 
-export async function sendMessage(message: TypeMessage) {
+export async function sendMessage(message: TypeMessage){
     const success = await messageRepository.sendMessage(message);
 
     if(!success){
         throw {type: "Method not allowed", message: "Message failed to be save!"};
     }
+}
+
+export async function getMessages(userId: number, friendId: number){
+    
 }
