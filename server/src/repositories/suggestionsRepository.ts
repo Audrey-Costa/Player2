ef9de4567086a2f480ec3bd3ae0fb81fbd7e0169
Audrey Costa <audreycota@nano.ufrj.br>
2022-10-09 21:16:24 -0300

    feat(back-end): add suggestions get service

---
+++ server/src/repositories/suggestionsRepository.ts
@@ -0,0 +1,7 @@
+export async function getRandomSuggestions(params:any): Promise<[]> {
+    return []
+}
+
+export async function getSuggestions(params:any): Promise<[]> {
+    return []
+}
--- server/src/services/suggestionsService.ts
+++ server/src/services/suggestionsService.ts
@@ -1,2 +1,22 @@
+import * as suggestionsRepository from "../repositories/suggestionsRepository"
+
 export async function getSuggestions(userId: number, random: any) {
+    if(random){
+        const suggestions = await suggestionsRepository.getRandomSuggestions(userId);
+
+        if(suggestions.length){
+            throw {type: "Not Found", message: "There are no users to suggest..."};
+        }
+
+        return suggestions;
+        
+    }else{
+        const suggestions = await suggestionsRepository.getSuggestions(userId);
+        
+        if(suggestions.length){
+            throw {type: "Not Found", message: "There are no users to suggest..."};
+        }
+        
+        return suggestions;
+    }
 }
